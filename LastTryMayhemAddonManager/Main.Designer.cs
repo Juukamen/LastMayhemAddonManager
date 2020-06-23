d056181af8c13e73351635a0bcfe656d6740059c
Peter Overeijnder <peterovereijnder@hotmail.com>
2020-06-23 23:48:24 +0200

    Added addon listing

--- LastTryMayhemAddonManager/Main.Designer.cs
+++ LastTryMayhemAddonManager/Main.Designer.cs
@@ -31,7 +31,10 @@
             this.gb_wow_installation = new System.Windows.Forms.GroupBox();
             this.rb_classic = new System.Windows.Forms.RadioButton();
             this.rb_retail = new System.Windows.Forms.RadioButton();
+            this.gb_myaddons = new System.Windows.Forms.GroupBox();
+            this.pnl_addonWrapper = new System.Windows.Forms.Panel();
             this.gb_wow_installation.SuspendLayout();
+            this.gb_myaddons.SuspendLayout();
             this.SuspendLayout();
             // 
             // gb_wow_installation
@@ -40,7 +43,7 @@
             this.gb_wow_installation.Controls.Add(this.rb_retail);
             this.gb_wow_installation.Location = new System.Drawing.Point(12, 12);
             this.gb_wow_installation.Name = "gb_wow_installation";
-            this.gb_wow_installation.Size = new System.Drawing.Size(776, 117);
+            this.gb_wow_installation.Size = new System.Drawing.Size(1234, 117);
             this.gb_wow_installation.TabIndex = 0;
             this.gb_wow_installation.TabStop = false;
             this.gb_wow_installation.Text = "World of Warcraft";
@@ -68,16 +71,36 @@
             this.rb_retail.UseVisualStyleBackColor = true;
             this.rb_retail.CheckedChanged += new System.EventHandler(this.WowClientCheckedChanged);
             // 
+            // gb_myaddons
+            // 
+            this.gb_myaddons.Controls.Add(this.pnl_addonWrapper);
+            this.gb_myaddons.Location = new System.Drawing.Point(12, 135);
+            this.gb_myaddons.Name = "gb_myaddons";
+            this.gb_myaddons.Size = new System.Drawing.Size(1234, 821);
+            this.gb_myaddons.TabIndex = 1;
+            this.gb_myaddons.TabStop = false;
+            this.gb_myaddons.Text = "My Addons";
+            // 
+            // pnl_addonWrapper
+            // 
+            this.pnl_addonWrapper.AutoScroll = true;
+            this.pnl_addonWrapper.Location = new System.Drawing.Point(6, 30);
+            this.pnl_addonWrapper.Name = "pnl_addonWrapper";
+            this.pnl_addonWrapper.Size = new System.Drawing.Size(1222, 785);
+            this.pnl_addonWrapper.TabIndex = 0;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1258, 968);
+            this.Controls.Add(this.gb_myaddons);
             this.Controls.Add(this.gb_wow_installation);
             this.Name = "Main";
             this.Text = "Form1";
             this.gb_wow_installation.ResumeLayout(false);
             this.gb_wow_installation.PerformLayout();
+            this.gb_myaddons.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -87,6 +110,8 @@
         private System.Windows.Forms.GroupBox gb_wow_installation;
         private System.Windows.Forms.RadioButton rb_classic;
         private System.Windows.Forms.RadioButton rb_retail;
+        private System.Windows.Forms.GroupBox gb_myaddons;
+        private System.Windows.Forms.Panel pnl_addonWrapper;
     }
 }
 
--- LastTryMayhemAddonManager/Main.cs
+++ LastTryMayhemAddonManager/Main.cs
@@ -3,6 +3,8 @@ using Microsoft.Win32;
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 
 namespace LastTryMayhemAddonManager
 {
@@ -63,9 +65,89 @@ namespace LastTryMayhemAddonManager
                 DirectoryInfo dir = this.installations[rb];
 
                 this.gb_wow_installation.Text = "World of Warcraft " + rb.Text + ": " + dir.FullName;
+                this.ListInstalledAddons(dir);
             }
         }
         #endregion //Events
+
+        private void ListInstalledAddons(DirectoryInfo dir)
+        {
+            DirectoryInfo addonsDir = new DirectoryInfo(dir.FullName + "\\Interface\\AddOns");
+            if(!addonsDir.Exists)
+            {
+                addonsDir.Create();
+            }
+
+            this.UpdateAddonList(addonsDir.GetDirectories());
+        }
+
+        private void UpdateAddonList(DirectoryInfo[] directories)
+        {
+            Control parent = this.pnl_addonWrapper;
+            parent.Controls.Clear();
+            Dictionary<int, List<Control>> components = new Dictionary<int, List<Control>>();
+
+            Label h1 = new Label();
+            h1.Text = "Addon";
+            h1.Font = new Font(h1.Font, FontStyle.Bold);
+            h1.Location = new Point(0, 0);
+            h1.Size = h1.PreferredSize;
+
+            Label h2 = new Label();
+            h2.Text = "Version";
+            h2.Font = new Font(h2.Font, FontStyle.Bold);
+            h2.Location = new Point(0, 0);
+            h2.Size = h2.PreferredSize;
+
+            components.Add(0, new List<Control>());
+            components.Add(1, new List<Control>());
+
+            components[0].Add(h1);
+            components[1].Add(h2);
+
+            parent.Controls.Add(h1);
+            parent.Controls.Add(h2);
+            foreach (DirectoryInfo dir in directories)
+            {
+                Label l1 = new Label();
+                l1.Text = dir.Name;
+                l1.Location = new Point(0, 0);
+                l1.Size = l1.PreferredSize;
+
+                Label l2 = new Label();
+                l2.Text = "-ni-";
+                l2.Location = new Point(0, 0);
+                l2.Size = l2.PreferredSize;
+
+                parent.Controls.Add(l1);
+                parent.Controls.Add(l2);
+
+                components[0].Add(l1);
+                components[1].Add(l2);
+            }
+
+            int xStart = 5;
+            int yStart = 5;
+
+            int xOffset = xStart;
+            int yOffset = yStart;
+
+            foreach(KeyValuePair<int, List<Control>> kvp in components)
+            {
+                foreach(Control control in kvp.Value)
+                {
+                    int idx = kvp.Value.IndexOf(control);
+                    int height = components.Select(x => x.Value[idx].Height).Max();
+
+                    control.Location = new Point(xOffset, yOffset);
+
+                    yOffset += height;
+                }
+
+                yOffset = yStart;
+                xOffset += kvp.Value.Select(x => x.Width).Max();
+            }
+        }
         #endregion //Private Methods
     }
 }
